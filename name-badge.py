70aceeda26408bf497a3d59cfc55d8fcb6fc9053
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-04-19 11:25:50 -0500

    Update name-badge.py
    added stretch/extra credit requirements

--- name-badge.py
+++ name-badge.py
@@ -5,6 +5,11 @@ email_address = input('And your email address? ')
 phone_number = input('Phone number? ')
 job_title = input('Alright, it\'s all coming together. What\'s your job title here at AS? ')
 id_number = input('And finally, what is your ID number? ')
+print(f'Thank you for input, {first_name}! All that\'s left is some extra identifiers:')
+hair_color = input('What is the color of your hair? ')
+eye_color = input('Your eyes? ')
+month = input('What month of this year is it now? ')
+b_has_training = input('Do you feel you have recieved sufficient training with Aperture Science\'s ID Badge Creator? ')
 
 print('\nPrinting new ID Badge...\n')
 
@@ -15,4 +20,7 @@ ID: {id_number}
 
 {email_address.lower()}
 {phone_number}
+
+Hair: {hair_color:14}  Eyes: {eye_color}
+Month: {month:13}  Training: {b_has_training}
 ----------------------------------------\n''')
