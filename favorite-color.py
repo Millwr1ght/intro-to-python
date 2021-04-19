3caa5143f0ab3b12225f9ee040d0a1229e28179d
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-04-19 10:25:58 -0500

    Create favorite-color.py

---
+++ favorite-color.py
@@ -0,0 +1,3 @@
+name = input('Hey there! What\'s your name? ')
+color = input(f'Nice to meet you {name.capitalize()}. What\'s your favorite color? ')
+print(f'Your favorite color is {color.lower()}? Mine too!')
