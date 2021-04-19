4535b401b177c1640a28614361af3a40a5d3b0dd
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-04-19 10:26:03 -0500

    Create names-bond.py

---
+++ names-bond.py
@@ -0,0 +1,4 @@
+first_name = input('What\'s your first name? ')
+last_name = input('Last name? ')
+
+print(f'\nWelcome {last_name.capitalize()}, {first_name.capitalize()} {last_name.capitalize()}.\nYour mission should you choose to accept it: Submit this assignment for me.\n')
