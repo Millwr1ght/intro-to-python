3990317ca433d4d1de9e0aa23cda7ea85635d560
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-05-05 15:58:27 -0500

    Create for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,13 @@
+# print each color in list, also 1-8 and 2-20
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+print()
+
+for num in range(1,9):
+    print(num)
+print()
+
+for num in range(2, 21, 2):
+    print(num)
