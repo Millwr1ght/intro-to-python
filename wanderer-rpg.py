e466cd8a09dbc23664bb658adb5af418a73e0b60
Millwr1ght <46363319+Millwr1ght@users.noreply.github.com>
2021-05-07 20:38:13 -0500

    Update wanderer-rpg.py

--- wanderer-rpg.py
+++ wanderer-rpg.py
@@ -9,6 +9,7 @@ has_awareness = False
 has_magic = False
 has_map = False
 adventurous = False
+stuck = False
 opening_sequence = '''----------------------------------------
                         _      _     
   \\  /\\  / /\\ |\\ | |`\\ |_ |^> |_ |^> 
@@ -28,7 +29,7 @@ credits_sequence = '''----------------------------------------
 
          Sound Design: Mill
             Visual FX: Mill
-            
+            QA Tester: Mill
 
     Made possible by users like you,
                Thank you!
@@ -36,86 +37,66 @@ credits_sequence = '''----------------------------------------
 ----------------------------------------
 '''
 
-menu_1 = '''----------------------------------------
+menu_default = '''----------------------------------------
  ======  WANDERER --- MAIN MENU  ====== 
 
      1. START Game   2. HOW To Play
      3. ABOUT Info   4. LEAVE 
 ----------------------------------------
 '''
-menu_2 = '''----------------------------------------
+menu_stuck = '''----------------------------------------
  ======  WANDERER --- MAIN MENU  ====== 
 
      1. START Game   2. HOW To Play
      3. ABOUT Info   no escape 
 ----------------------------------------
 '''
+menu_what = '''----------------------------------------
+ ======  WANDERER --- MAIN MENU  ====== 
 
+     1. What GAME    2. HOW To Play
+     3. ABOUT Info   4. LEAVE 
+----------------------------------------
+'''
+menu_what_stuck = '''----------------------------------------
+ ======  WANDERER --- MAIN MENU  ====== 
+
+     1. what game    2. HOW To PLAY
+     3. ABOUT Info   no escape 
+----------------------------------------
+'''
 #opening title sequence
 print(opening_sequence)
+current_menu = menu_default
 
 while gamemode_menu:
 #menu
-    menu_choice = input(menu_1)
+    menu_choice = input(current_menu)
     print('----------------------------------------')
+
 #about screen
     if menu_choice.lower() == 'about' or menu_choice == '3':
         print(' =========  ABOUT - WANDERER  ========= \nMillwright Gaming is a one-person game\ndevelopment team devoted to making high\nquality gaming experiences available at\naffordable prices. This version of\nWANDERER is the 2021 reboot of the 2015\nclassic, originally available on Mill\'s\nTI-84 Graphing Calculator.')
+
 #help screen
     elif menu_choice.lower() == 'help' or menu_choice.lower() == 'how' or menu_choice == '2':
         print(' ===========  HOW TO PLAY  ============\nAs you journey across the DESERTS of\nINCARNADINE, you will be presented with\nmany CHOICES, which will be displayed in\nFULLCAPS. You can then type the choice\nyou choose and suffer the consequences,\nwhich may lead to more choices.\n\nYour goal? EXPLORATION, TREASURE, and\nabove all, ADVENTURE.')
         has_awareness = True
+
 #exit game
     elif menu_choice.lower() == 'leave' or menu_choice.lower() == 'x' or menu_choice.lower() == 'exit' or menu_choice == '4':
-        print('\n           Thanks for playing!\n\n\n\n\n\n')
-        if has_awareness and not adventurous:
-            print('...\n\n\n\n')
-            print('...wait a second')
-            print('You can\'t just QUIT! You haven\'t achieved the goals yet!')
-            game_choice = input('There is still much ADVENTURE to be had! So much TREASURE!\n')
-            if game_choice.lower() == 'adventure' or game_choice.lower() == 'treasure':
-                print('I knew you would come around! I\'ll take you back to the MENU so you can start your ADVENTURE.')
-                adventurous = True
-            elif game_choice.lower() == 'quit':
-                print('You think you\'re funny, eh? You think this is a joke!?')
-                print('THERE IS NO ESCAPE! ONLY ADVENTURE!\n')
-                print('in its fury, the game kills you.\nGAME OVER')
-                game_running = False
-            elif game_choice.lower() == 'sudo quit' or game_choice.lower() == 'sudo leave' or game_choice.lower() == 'sudo exit' or game_choice.lower() == 'sudo 4' or game_choice.lower() == 'sudo x':
-                print('Ah. I see. As you were.')
-                game_running = False
-            else:
-                print('Giving me that NONSENSE will do you no good on your ADVENTURE. Here, I will take you back to the MAIN MENU where you can start over.')
-                has_nonsense = True
-        elif adventurous:
-            print('...\n\n\n\n')
-            print('...wait a second')
-            game_choice = input('You can\'t just QUIT! What happened to wanting to go on your ADVENTURE? You think I will just LET you leave? Do you think this is a GAME?\n')
-            if game_choice.lower() == 'adventure' or game_choice.lower() == 'treasure':
-                print('I knew you would come around! I\'ll take you back to the MENU so you can start your ADVENTURE.')
-                adventurous = True
-            elif game_choice.lower() == 'quit' or game_choice.lower() == 'let':
-                print('You think you\'re funny, eh? You think this is a joke!?')
-                print('THERE IS NO ESCAPE! ONLY ADVENTURE!\n')
-                print('in its fury, the game kills you.\nGAME OVER')
-                game_running = False
-            elif game_choice.lower() == 'game':
-                game_choice = input('I have some news for you buddy, it isn\'t a game. It is a text-based RPG adventure. See? NOT a game, an ADVENTURE!\n')
-                if game_choice.lower() == 'adventure':
-                    print('I knew you would come around! I\'ll take you back to the MENU so you can start your ADVENTURE.')
-                elif game_choice.lower() == 'not' or game_choice.lower() == 'no':
-                    print('That\'s right.\nNO ESCAPE\n')
-                else:
-                    print('Giving me that NONSENSE will do you no good on your ADVENTURE. Here, I will take you back to the MAIN MENU where you can start over.')
-                    has_nonsense = True
-            else:
-                print('Giving me that NONSENSE will do you no good on your ADVENTURE. Here, I will take you back to the MAIN MENU where you can start over.')
-                has_nonsense = True 
-        else:
-            game_running = False
+        print('\n\t\tThanks for playing!\n')
+        game_running = False
+
 #adventure begins
     elif menu_choice == '1' or menu_choice.lower() == 'start' or menu_choice.lower == 'adventure':
         gamemode_menu = False
+        gamemode_menu = True
+        """ ha. you thought. """
+        if stuck:
+            current_menu = menu_what_stuck
+        else:
+            current_menu = menu_what
 
 #credits
     elif menu_choice.upper() == 'WANDERER':
@@ -129,6 +110,8 @@ while gamemode_menu:
     else:
         #not a menu choice
         print('Please make a valid selection\n')
+
+
 
 #gameplay begins
 print('\n\nYou are jostled awake.')
